115a5672bc3b106855fda35149011f7b1ef8df9e
Guillaume Rio <guigzrio@gmail.com>
2016-08-09 10:36:57 +0200

    Create packages, main class and intelliJ config files

---
+++ src/com/objis/formationjava/d20160809/presentation/Launcher.java
@@ -0,0 +1,12 @@
+package com.objis.formationjava.d20160809.presentation;
+
+/**
+ * Created by Stagiaire on 09/08/2016.
+ */
+public class Launcher {
+
+    public static void main(String[] args){
+
+    }
+
+}
